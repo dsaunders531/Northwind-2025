81fab8fb6feab63b0aa4af60eee9d7421e17d889
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-01 19:05:07 +0000

    Add sql server implementations of context and service

---
+++ Northwind.Context.MsSql/Contexts/NorthwindContextSql.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Context.Contexts;
+
+namespace Northwind.Context.MsSql.Contexts
+{
+    public partial class NorthwindContextSql : NorthwindContext
+    {
+        public NorthwindContextSql()
+        {
+        }
+
+        public NorthwindContextSql(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LAPTOP10\\SQLEXPRESS;Database=Northwind-2025-Local;Trusted_Connection=true;MultipleActiveResultSets=true;");
+            }
+        }        
+    }
+}
---
+++ Northwind.Context.MsSql/Services/NorthwindServiceSql.cs
@@ -0,0 +1,118 @@
+﻿using Northwind.Context.Interfaces;
+using Northwind.Context.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Context.MsSql.Services
+{
+    public class NorthwindServiceSql : INorthwindService
+    {
+        public IList<AlphabeticalListOfProduct> AlphabeticalListOfProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CategorySalesFor1997> CategorySalesFor1997s()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CurrentProductList> CurrentProductLists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CustomerAndSuppliersByCity> CustomerAndSuppliersByCities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CustomerOrders> CustomerOders(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CustomerOrderDetail> CustomerOrderDetail(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CustomerOrderHistory> CustomerOrderHistory(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EmployeeSalesByCountry> EmployeeSalesByCountries(DateTime fromDate, DateTime toDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Invoice> Invoices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<OrderDetailsExtended> OrderDetailsExtendeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<OrdersQry> OrdersQries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<OrderSubtotal> OrderSubtotals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductsAboveAveragePrice> ProductsAboveAveragePrices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductSalesFor1997> ProductSalesFor1997s()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductsByCategory> ProductsByCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SalesByCategory> SalesByCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SaleByYear> SalesByYear(DateTime fromDate, DateTime toDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SalesTotalsByAmount> SalesTotalsByAmounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SummaryOfSalesByQuarter> SummaryOfSalesByQuarters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SummaryOfSalesByYear> SummaryOfSalesByYears()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<MostExpensiveProduct> TenMostExpensiveProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
