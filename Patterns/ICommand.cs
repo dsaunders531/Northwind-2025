d749c15d0854eddc87e7dd081b0a60f2fecc53f5
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-06 14:48:13 +0000

    Add a library to store bases for any patterns we might use

---
+++ Patterns/ICommand.cs
@@ -0,0 +1,76 @@
+﻿namespace Patterns
+{
+    /// <summary>
+    /// The command pattern.
+    /// </summary>
+    /// <remarks>When implementing, the constructor contains the parameters.</remarks>    
+    public interface ICommand
+    {
+        /// <summary>
+        /// Run something
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// Undo what run did.
+        /// </summary>
+        /// <remarks>Throw an exception if the process does not work correctly.</remarks>
+        void Undo();
+    }
+
+    /// <summary>
+    /// The command pattern.
+    /// </summary>
+    /// <remarks>When implementing, the constructor contains the parameters.</remarks>  
+    public interface IAsyncCommand
+    {
+        /// <summary>
+        /// Run something
+        /// </summary>
+        Task Run();
+
+        /// <summary>
+        /// Undo what run did.
+        /// </summary>
+        /// <remarks>Throw an exception if the process does not work correctly.</remarks>
+        Task Undo();
+    }
+
+    /// <summary>
+    /// The command pattern.
+    /// </summary>
+    /// <remarks>When implementing, the constructor contains the parameters.</remarks> 
+    public interface ICommand<TOutput>
+        where TOutput : class
+    {
+        /// <summary>
+        /// Run something
+        /// </summary>
+        TOutput Run();
+
+        /// <summary>
+        /// Undo what run did.
+        /// </summary>
+        /// <remarks>Throw an exception if the process does not work correctly.</remarks>
+        void Undo();
+    }
+
+    /// <summary>
+    /// The command pattern.
+    /// </summary>
+    /// <remarks>When implementing, the constructor contains the parameters.</remarks> 
+    public interface IAsyncCommand<TOutput>
+        where TOutput : class
+    {
+        /// <summary>
+        /// Run something
+        /// </summary>
+        Task<TOutput> Run();
+
+        /// <summary>
+        /// Undo what run did.
+        /// </summary>
+        /// <remarks>Throw an exception if the process does not work correctly.</remarks>
+        Task Undo();
+    }
+}
---
+++ Patterns/StartAndEndDate.cs
@@ -0,0 +1,23 @@
+﻿namespace Patterns
+{
+    /// <summary>
+    /// Base class which need a start and end date.
+    /// </summary>
+    /// <remarks>Implements standard behavoir - start is always at 00:00 and end is always at 23:59 on the day.</remarks>
+    public class StartAndEndDate
+    {
+        private DateTime _startDate;
+
+        private DateTime _endDate;
+
+        /// <summary>
+        /// Always return the start date as the start of the day.
+        /// </summary>
+        public DateTime StartDate { get => this._startDate.Date; set => this._startDate = value; }
+
+        /// <summary>
+        /// Always return the end date as the last moment of the day.
+        /// </summary>
+        public DateTime EndDate { get => this._endDate.Date.AddDays(1).AddTicks(-1); set => this._endDate = value; }
+    }
+}
