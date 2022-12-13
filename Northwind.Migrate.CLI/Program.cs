e630f65ee3d9b9199689c53a711e65a45d467492
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-13 18:03:55 +0000

    Add migration console app

---
+++ Northwind.Migrate.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind.Migrate.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            throw new NotImplementedException();
+        }
+    }
+}
