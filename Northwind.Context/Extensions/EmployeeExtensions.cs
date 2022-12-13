8c5052f904801d87bb0c775bbc0ec81a7ec7bf03
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-13 18:00:31 +0000

    Add extensions for common data methods

---
+++ Northwind.Context/Extensions/EmployeeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Northwind.Context.Models;
+
+namespace Northwind.Context.Extensions
+{
+
+    internal static class EmployeeExtensions
+    {
+        public static string FullName(this Employee employee)
+        {
+            return string.Concat(employee.FirstName ?? string.Empty, " ", employee.LastName ?? string.Empty);
+        }
+    }
+}
---
+++ Northwind.Context/Extensions/OrderDetailsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Northwind.Context.Models;
+
+namespace Northwind.Context.Extensions
+{
+    internal static class OrderDetailsExtensions
+    {
+        /// <summary>
+        /// This formula is used often.
+        /// </summary>
+        /// <param name="orderdetail"></param>
+        /// <returns></returns>
+        public static decimal? ExtendedPrice(this OrderDetail orderdetail)
+        {
+            return Math.Round((orderdetail.UnitPrice * orderdetail.Quantity * (decimal)((1 - orderdetail.Discount) / 100)) * 100, 2);
+        }
+    }
+}
