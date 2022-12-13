2f9b8e3f665630595c933888346e8719188851bc
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-13 18:03:28 +0000

    Add seed data functions

---
+++ Northwind.Context/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Northwind.Context.Contexts;
+
+namespace Northwind.Context
+{
+    public static class SeedData
+    {
+        /// <summary>
+        /// Move the dates forward so the data becomes more useful.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void BringUpToDate(this NorthwindContext context, DateTime maxDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
