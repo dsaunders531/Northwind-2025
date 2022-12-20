221509bb2454a4b4909e509a48be2d625d620e0c
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-12-20 14:47:13 +0000

    Add api project

---
+++ Northwind.Api/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Duncan Saunders">
+// Copyright (c) Duncan Saunders. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1413:Use trailing comma in multi-line initializers", Justification = "Makes code less readable")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Makes code less readable")]
