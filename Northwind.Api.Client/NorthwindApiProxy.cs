4fdea530b0f8cb59c1bd731eb82461e935244bca
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2023-01-05 18:01:29 +0000

    Add api client (to use as a proxy or in other projects)

---
+++ Northwind.Api.Client/NorthwindApiProxy.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Configuration;
+using Northwind.Context.Interfaces;
+using Northwind.Context.Models.Api;
+using Patterns;
+using System.Net.Http.Json;
+
+namespace Northwind.Api.Client
+{
+    /// <summary>
+    /// An client which acts as a proxy to the real api.
+    /// </summary>
+    /// <remarks>The configuration needs a setting at top-level "ApiBaseUrl".</remarks>
+    public class NorthwindApiProxy : INorthwindProductsService
+    {
+        public NorthwindApiProxy(HttpClient httpClient, IConfiguration configuration)
+        {
+            this.Client = httpClient;
+            this.BaseUrl = new Uri(configuration["ApiBaseUrl"].ToString());
+
+            if (string.IsNullOrWhiteSpace(this.BaseUrl.Host))
+            {
+                throw new UriFormatException("Url does not look correct. Check the configuration.");
+            }
+        }
+
+        private HttpClient Client { get; set; }
+
+        private Uri BaseUrl { get; set; }
+
+        private void BeforeAction()
+        {
+            this.Client.BaseAddress = this.BaseUrl;
+            this.Client.DefaultRequestHeaders.Clear(); // There are none right now.
+            this.Client.Timeout = TimeSpan.FromSeconds(3d);
+        }
+
+        public async Task<global::Patterns.IPagedResponse<CategoryApi>> GetCategories(int page, global::Patterns.SortBy sort)
+        {
+            this.BeforeAction();
+
+            HttpResponseMessage response = await this.Client.GetAsync(new Uri(this.BaseUrl, $"Categories?page={page}&sort={sort}"));
+
+            response.EnsureSuccessStatusCode();
+
+            return (await response.Content.ReadFromJsonAsync<PagedResponse<CategoryApi>>() ?? new PagedResponse<CategoryApi>()) as IPagedResponse<CategoryApi>;
+        }
+
+        public async Task<ProductApi?> GetProductById(int productId)
+        {
+            this.BeforeAction();
+
+            HttpResponseMessage response = await this.Client.GetAsync(new Uri(this.BaseUrl, $"Products/{productId}"));
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return default;
+            }
+            else
+            {
+                return (await response.Content.ReadFromJsonAsync<ProductApi>());
+            }
+        }
+
+        public async Task<global::Patterns.IPagedResponse<ProductApi>> GetProducts(int page, global::Patterns.SortBy sort, string searchTerm)
+        {
+            this.BeforeAction();
+
+            HttpResponseMessage response = await this.Client.GetAsync(new Uri(this.BaseUrl, $"Products?page={page}&sort={sort}&searchTerm={searchTerm}"));
+
+            response.EnsureSuccessStatusCode();
+
+            return (await response.Content.ReadFromJsonAsync<PagedResponse<ProductApi>>() ?? new PagedResponse<ProductApi>()) as IPagedResponse<ProductApi>;
+        }
+
+        public async Task<global::Patterns.IPagedResponse<ProductApi>> GetProductsInCategory(int categoryId, int page, global::Patterns.SortBy sort)
+        {
+            this.BeforeAction();
+
+            HttpResponseMessage response = await this.Client.GetAsync(new Uri(this.BaseUrl, $"Categories/{categoryId}/products?page={page}&sort={sort}"));
+
+            response.EnsureSuccessStatusCode();
+
+            return (await response.Content.ReadFromJsonAsync<PagedResponse<ProductApi>>() ?? new PagedResponse<ProductApi>()) as IPagedResponse<ProductApi>;
+        }
+
+        public async Task<string[]> SearchProducts(string term)
+        {
+            this.BeforeAction();
+
+            HttpResponseMessage response = await this.Client.GetAsync(new Uri(this.BaseUrl, $"Products/search?term={term}"));
+
+            response.EnsureSuccessStatusCode();
+
+            return (await response.Content.ReadFromJsonAsync<string[]>() ?? Array.Empty<string>());
+        }
+    }
+}
