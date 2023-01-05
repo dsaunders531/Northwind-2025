6ff8d2c3cd438540939b740d2f8f2033c781bd4d
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2023-01-05 18:03:21 +0000

    Add api to web project (because of cross-origin issues)

---
+++ Northwind.Web/Areas/Api/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Context.Interfaces;
+using Northwind.Context.Models.Api;
+using Northwind.Definitions.Interfaces;
+using Patterns;
+
+namespace Northwind.Web.Areas.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CategoriesController: ControllerBase, ICategoriesController
+    {
+        public CategoriesController(INorthwindProductsService service, ILogger<CategoriesController> logger)
+        {
+            Service = service;
+            Logger = logger;
+        }
+
+        private INorthwindProductsService Service { get; set; }
+
+        private ILogger<CategoriesController> Logger { get; set; }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IPagedResponse<CategoryApi>))]
+        public async Task<ActionResult<IPagedResponse<CategoryApi>>> Categories([FromQuery] int page = 1, [FromQuery] SortBy sort = SortBy.NameAscending)
+        {
+            try
+            {
+                return new JsonResult(await Service.GetCategories(page, sort));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Error in GET Categories. {ex.Message}");
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("{categoryId}/products")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IPagedResponse<ProductApi>))]
+        public async Task<ActionResult<IPagedResponse<ProductApi>>> ProductsInCategories([FromRoute] int categoryId, [FromQuery] int page = 1, [FromQuery] SortBy sort = SortBy.NameAscending)
+        {
+            try
+            {
+                return new JsonResult(await Service.GetProductsInCategory(categoryId, page, sort));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Error in GET Products in Categories. {ex.Message}");
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
