7b7edebb16932cd4333ac723e38ec7300e27e853
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2023-02-15 16:17:06 +0000

    Update tests

--- Northwind.Reporting.Rcl/Reports/SalesByCategoryReport.cs
+++ Northwind.Reporting.Rcl/Reports/SalesByCategoryReport.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Northwind.Context.Interfaces;
 using Northwind.Context.Models.Reporting;
+using Northwind.Reporting.Extensions;
 using Northwind.Reporting.Interfaces;
+using Northwind.Reporting.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Reporting.Rcl.Reports
@@ -12,7 +14,7 @@ namespace Northwind.Reporting.Rcl.Reports
 
         public override string Name => "Sales By Category";
 
-        public override string Description => "Show the sales by category between dates.";
+        public override string Description => "Show the sales by category for a year.";
 
         public override string ConfigPageRoute => "/Reporting/Config/SalesByCategoryAndYear";
 
@@ -20,17 +22,20 @@ namespace Northwind.Reporting.Rcl.Reports
         {
             INorthwindService northwindService = this.ServiceProvider.GetRequiredService<INorthwindService>();
 
+            // not really relevant here - since year is the only parameter we are interested in.
+            parameters.CalculateStartAndEndDates();
+
             return (await northwindService.SalesByCategory(parameters.Category, parameters.Year)).AsEnumerable();
         }
     }
 
-    public class SalesByCategoryAndYearReportParameters
+    public class SalesByCategoryAndYearReportParameters : ReportParametersBase
     {
         public SalesByCategoryAndYearReportParameters()
         {
             this.Year = DateTime.UtcNow.Year;
         }
-
+        
         [Required]
         public string Category { get; set; } = string.Empty;
 
--- Northwind.Reporting.Rcl/Reports/SalesTotalsByAmountsReport.cs
+++ Northwind.Reporting.Rcl/Reports/SalesTotalsByAmountsReport.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Northwind.Context.Interfaces;
 using Northwind.Context.Models.Reporting;
+using Northwind.Reporting.Extensions;
 using Northwind.Reporting.Interfaces;
+using Northwind.Reporting.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Reporting.Rcl.Reports
@@ -19,16 +21,15 @@ namespace Northwind.Reporting.Rcl.Reports
         public override async Task<IEnumerable<SalesTotalsByAmount>> Run(SalesTotalsByAmountsReportParameters parameters)
         {
             // Make sure the dates cover all the time on selected days
-            parameters.StartDate = parameters.StartDate.Date;
-            parameters.EndDate = parameters.EndDate.Date.AddDays(1).AddTicks(-1); // the last moment of the day
-
+            parameters.CalculateStartAndEndDates();
+            
             INorthwindService service = this.ServiceProvider.GetRequiredService<INorthwindService>();
 
-            return (await service.SalesTotalsByAmounts(parameters.StartDate, parameters.EndDate)).AsEnumerable();
+            return (await service.SalesTotalsByAmounts(parameters.StartDate.Value, parameters.EndDate.Value)).AsEnumerable();
         }
     }
 
-    public class SalesTotalsByAmountsReportParameters
+    public class SalesTotalsByAmountsReportParameters : ReportParametersBase
     {
         public SalesTotalsByAmountsReportParameters()
         {
@@ -36,14 +37,6 @@ namespace Northwind.Reporting.Rcl.Reports
 
             this.StartDate = now.AddDays((now.Day * -1) + 1).AddMonths(-1);
             this.EndDate = now.AddDays(now.Day * -1);
-        }
-
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime StartDate { get; set; }
-
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime EndDate { get; set; }
+        }        
     }
 }
