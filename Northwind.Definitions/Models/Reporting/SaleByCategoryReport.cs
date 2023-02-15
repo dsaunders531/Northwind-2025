bef9866f783b4422839f0cf06aa36d7a905165e3
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2023-02-15 16:14:58 +0000

    Update attributes on report models

--- Northwind.Definitions/Models/Reporting/SaleByCategoryReport.cs
+++ Northwind.Definitions/Models/Reporting/SaleByCategoryReport.cs
@@ -2,12 +2,24 @@
 // Copyright (c) Duncan Saunders. All rights reserved.
 // </copyright>
 
+using ClosedXML.Attributes;
+using FileHelpers;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+
 namespace Northwind.Context.Models.Reporting
 {
+    [DelimitedRecord(",")]
+    [Keyless]
     public class SaleByCategoryReport
     {
+        [XLColumn(Order = 1, Header = "Product Name")]
+        [FieldOrder(1)]
         public string? ProductName { get; set; }
 
+        [XLColumn(Order = 2, Header = "Total Purchased")]
+        [FieldOrder(2)]
+        [FieldConverter(ConverterKind.Decimal,".")]
         public decimal TotalPurchased { get; set; }
     }
 }
--- Northwind.Definitions/Models/Reporting/SalesTotalsByAmount.cs
+++ Northwind.Definitions/Models/Reporting/SalesTotalsByAmount.cs
@@ -2,24 +2,37 @@
 // Copyright (c) Duncan Saunders. All rights reserved.
 // </copyright>
 
+using ClosedXML.Attributes;
+using FileHelpers;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Northwind.Context.Models.Reporting
 {
+    [DelimitedRecord(",")]
     [Keyless]
     public partial class SalesTotalsByAmount
     {
+        [XLColumn(Order = 4, Header = "Sale Amount")]
+        [FieldOrder(4)]
+        [FieldConverter(ConverterKind.Decimal,".")]
         [Column(TypeName = "money")]
         public decimal? SaleAmount { get; set; }
 
+        [XLColumn(Order = 1, Header = "Order Id")]
+        [FieldOrder(1)]
         [Column("OrderID")]
         public int OrderId { get; set; }
 
+        [XLColumn(Order = 2, Header = "Company Name")]
+        [FieldOrder(2)]
         [StringLength(40)]
         public string CompanyName { get; set; } = null!;
 
+        [XLColumn(Order = 3, Header = "Shipped Date")]
+        [FieldOrder(3)]
+        [FieldConverter(ConverterKind.Date, "dd-MMM-yyyy")]
         [Column(TypeName = "datetime")]
         public DateTime? ShippedDate { get; set; }
     }
