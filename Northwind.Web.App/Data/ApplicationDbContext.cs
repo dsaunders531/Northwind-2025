64cd51d2bfc0221edb01e861ee4cdb50c71d7791
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2023-12-28 10:57:38 +0000

    Add default angular project

---
+++ Northwind.Web.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Northwind.Web.App.Models;
+
+namespace Northwind.Web.App.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Northwind.Web.App/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Northwind.Web.App.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
