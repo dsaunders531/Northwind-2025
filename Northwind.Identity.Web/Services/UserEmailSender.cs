56927a3a9579b5bdcb347251dfc1315ab967fdca
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2024-08-10 18:41:51 +0100

    Add IEmailSender<ApplicationUser> this is a new thing for identity where the system emails are seperate so they can be branded and formatted well before sending

---
+++ Northwind.Identity.Web/Services/UserEmailSender.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Northwind.Identity.Web.Models;
+
+namespace Northwind.Identity.Web.Services
+{
+    public class UserEmailSender : IEmailSender<ApplicationUser>
+    {
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+        {
+            // TODO
+            // HINT - instead of blocking while sending a message, put the message in a queue instead.
+            // Then another process (or event handler) can send the message.
+            // This is better for site performance as there are no blocking calls this way.
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
